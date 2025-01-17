e25d8cb0becbd851c57d67d600bafd3ef51ca98c
Himani Patel <himanipatel.riya@gmail.com>
2025-01-17 14:33:05 +0530

    day 4

---
+++ 17 jan/debouncefunction.js
@@ -0,0 +1,19 @@
+function debounce(func, delay) {
+    let timeoutId;
+    
+    return function (...args) {
+        clearTimeout(timeoutId);
+        
+        timeoutId = setTimeout(() => {
+            func(...args);
+        }, delay);
+    };
+}
+
+const handleResize = () => {
+    console.log("Window resized!");
+};
+
+const debouncedResize = debounce(handleResize, 500);
+
+window.addEventListener("resize", debouncedResize);
---
+++ 17 jan/groupanagrams.js
@@ -0,0 +1,15 @@
+function groupAnagrams(strs) {
+    let map = new Map();
+    
+    for (let str of strs) {
+        let sortedStr = str.split('').sort().join('');
+        
+        if (!map.has(sortedStr)) {
+            map.set(sortedStr, []);
+        }
+        
+        map.get(sortedStr).push(str);
+    }
+    
+    return Array.from(map.values());
+}
